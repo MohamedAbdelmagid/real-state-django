197413238c68af87e1cf7ad81e5ecf67908d1d53
MohamedAbdelmagid <mohamed.abdelmagid.1991@gmail.com>
2020-11-21 15:46:37 -0600

    Add a templates folder

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('<h2>Hi Django!</h2>')
+    return render(request, 'pages/index.html')
+
+def about(request):
+    return render(request, 'pages/about.html')
+

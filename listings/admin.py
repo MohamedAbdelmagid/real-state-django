6fff38513a35e50ebb691649417e5975ba12b172
MohamedAbdelmagid <mohamed.abdelmagid.1991@gmail.com>
2020-11-23 15:08:16 -0600

    Customize tables in the admin page

--- listings/admin.py
+++ listings/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
+
 from . models import Listing
 
 
-admin.site.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+    """ Custom listing table in the admin page """
+
+    list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
+    list_display_links = ('id', 'title')
+    list_filter = ('realtor',)
+    list_editable = ('is_published',)
+    search_fields = ('title', 'description', 'address', 'city', 'state', 'zipcode', 'price')
+
+
+admin.site.register(Listing, ListingAdmin)

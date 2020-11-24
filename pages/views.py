58bdb7d78ca8448a21ecad6a743c2393d4864e8e
MohamedAbdelmagid <mohamed.abdelmagid.1991@gmail.com>
2020-11-24 17:02:59 -0600

    Display realtors & mvp in the about template

--- pages/views.py
+++ pages/views.py
@@ -11,5 +11,12 @@ def index(request):
     return render(request, 'pages/index.html', context)
 
 def about(request):
-    return render(request, 'pages/about.html')
+    realtors = Realtor.objects.order_by('-hire_date')
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+
+    context = {
+        'realtors': realtors,
+        'mvp_realtors': mvp_realtors,
+    }
+    return render(request, 'pages/about.html', context)
 
--- realtors/admin.py
+++ realtors/admin.py
@@ -6,8 +6,9 @@ from . models import Realtor
 class RealtorAdmin(admin.ModelAdmin):
     """ Custom realtor table in the admin page """
 
-    list_display = ('id', 'name', 'email', 'hire_date')
+    list_display = ('id', 'name', 'is_mvp', 'email', 'hire_date')
     list_display_links = ('id', 'name')
+    list_editable = ('is_mvp',)
     search_fields = ('name',)
     list_per_page = 20
 

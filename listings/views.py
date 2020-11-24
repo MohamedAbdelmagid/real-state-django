37ffa2e131a293981dd8e31f777c0bb5c4156d87
MohamedAbdelmagid <mohamed.abdelmagid.1991@gmail.com>
2020-11-24 17:03:26 -0600

    Display single listing

--- listings/views.py
+++ listings/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 from . models import Listing
@@ -14,8 +14,11 @@ def index(request):
     context = { 'listings': paged_listings }
     return render(request, 'listings/listings.html', context)
 
-def listing(request):
-    return render(request, 'listings/listing.html')
+def listing(request, id):
+    listing = get_object_or_404(Listing, pk=id)
+
+    context = { 'listing': listing }
+    return render(request, 'listings/listing.html', context)
 
 def search(request):
     return render(request, 'listings/search.html')

ce333f3338c8ea59602ca8d6d4628bea6e404e34
MohamedAbdelmagid <mohamed.abdelmagid.1991@gmail.com>
2020-11-26 06:45:02 -0600

    Implement Login

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
 from django.contrib.auth.models import User
 
 
@@ -41,6 +41,17 @@ def dashboard(request):
     return render(request, 'accounts/dashboard.html')
 
 def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are logged in! find your dream property.')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid Credentials')
     return render(request, 'accounts/login.html')
 
 def logout(request):
